b87390b7a34decd19623a8715b8968a3cc2cd499
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-20 12:14:06 -0700

    added cubes+ and x+ functionality

--- arithmetic.py
+++ arithmetic.py
@@ -47,3 +47,17 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+
+def add_mult(num1, num2, num3):
+    """Return the total from adding num1 and num2 and multipling sum by num3"""
+
+    return (num1 + num2) * num3
+
+
+def add_cubes(num1, num2):
+    """Return the sum of the cubes of num1 and num2"""
+
+    return cube(num1) + cube(num2)
+
+    
